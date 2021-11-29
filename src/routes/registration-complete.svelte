451412d89a257d0cbf0ed5e62d9f765cfd14c8fd
Naved Islam <navedcoded@gmail.com>
2021-11-29 20:03:26 +0200

    Create registration-complete.svelte

---
+++ src/routes/registration-complete.svelte
@@ -0,0 +1,8 @@
+<script>
+
+import { add_classes } from "svelte/internal";
+
+</script>
+
+<h1>Thank you for registering</h1>
+<p>You will be contacted soonby email to get started with your class, inshaAllah.</p>
