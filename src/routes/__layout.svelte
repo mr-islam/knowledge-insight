f009c9c3ac77d1f726b51768aa48fd777157e97e
Naved Islam <navedcoded@gmail.com>
2021-11-29 19:06:11 +0200

    formatting

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,35 +1,44 @@
 <script context="module">
-  import { t, locale, locales, register, addMessages, init, getLocaleFromNavigator /*, register */ } from 'svelte-intl-precompile';
-  import { session } from '$app/stores';
-  import en from '$locales/en.js';
-  import ar from '$locales/ar.js';
+  import {
+    t,
+    locale,
+    locales,
+    register,
+    addMessages,
+    init,
+    getLocaleFromNavigator /*, register */,
+  } from "svelte-intl-precompile";
+  import { session } from "$app/stores";
+  import en from "$locales/en.js";
+  import ar from "$locales/ar.js";
 
-  addMessages('en', en);
+  addMessages("en", en);
   // addMessages('ar', ar);
   // register('en', () => import('$locales/en.js'));
-  register('ar', () => import('$locales/ar.js'));
+  register("ar", () => import("$locales/ar.js"));
 
-
-	import '../app.css';  
-
+  import "../app.css";
 </script>
 
 <script>
   init({
-    fallbackLocale: 'en',
-    initialLocale: getLocaleFromNavigator($session.acceptedLanguage)
-  });	
+    fallbackLocale: "en",
+    initialLocale: getLocaleFromNavigator($session.acceptedLanguage),
+  });
 </script>
 
 <header>
-  <select id="select-lang" on:change={() => {$locale = document.getElementById('select-lang').selectedOptions[0].value; }}>
+  <select
+    id="select-lang"
+    on:change={() => {
+      $locale = document.getElementById("select-lang").selectedOptions[0].value;
+    }}
+  >
     {#each $locales as loc}
-        <option value={loc}>{loc}</option>
+      <option value={loc}>{loc}</option>
     {/each}
   </select>
 </header>
-
-
 
 <main>
   <div style="display: flex; justify-content: center;">
@@ -38,22 +47,23 @@
     </a>
   </div>
 
-	<slot />
+  <slot />
 </main>
 
 <footer>
   <div style="text-align: center;">
-    {$t('title')}
-    *
-    <a href="mailto:ilm.marifah.center@gmail.com">{ $t('contact_us') }</a>
+    <a href="mailto:ilm.marifah.center@gmail.com">{$t("contact_us")}</a>
   </div>
-  <script defer data-domain="ilm-marifah.com" src="https://plausible.io/js/plausible.js"></script>
+  <script
+    defer
+    data-domain="ilm-marifah.com"
+    src="https://plausible.io/js/plausible.js"></script>
 </footer>
 
 <style>
   img#logo {
-        border: 2px black solid;
-        border-radius: 360px;
-        max-width: 10vw;
-    }
+    border: 2px black solid;
+    border-radius: 360px;
+    max-width: 10vw;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,30 @@
 <script>
-   	import { t, locale, locales } from 'svelte-intl-precompile'
-    import CourseCard from '../components/CourseCard.svelte'
+  import { t, locale, locales } from "svelte-intl-precompile";
+  import CourseCard from "../components/CourseCard.svelte";
 </script>
 
 {#if t}
+  <div class="first-section">
+    <h1>{$t("title")}</h1>
 
-    <div class="first-section"> 
-        <h1>{$t('title')}</h1>
-
-        <div class="about">
-                <img alt="The Holy Mosque of Madinah" class="img-hero-side" src="/backgrounds/madina-busy.jpg" />
-                <div class="paras-about">
-                    <p>{ $t('welcome.para_1') }</p>
-                    <p>{ $t('welcome.para_2') } </p>
-                    <p>{ $t('welcome.para_3') }</p>                
-            </div>
-         </div>
+    <div class="about">
+      <img
+        alt="The Holy Mosque of Madinah"
+        class="img-hero-side"
+        src="/backgrounds/madina-busy.jpg"
+      />
+      <div class="paras-about">
+        <p>{$t("welcome.para_1")}</p>
+        <p>{$t("welcome.para_2")}</p>
+        <p>{$t("welcome.para_3")}</p>
+      </div>
     </div>
+  </div>
 
-    
-
-    <div class="main">
-        <h1>{ $t('courses.title') }</h1>
-        <div class="gallery">
-            <!-- <CourseCard 
+  <div class="main">
+    <h1>{$t("courses.title")}</h1>
+    <div class="gallery">
+      <!-- <CourseCard 
                     title={""} 
                     desc={""} 
                     src={""} 
@@ -32,84 +33,83 @@
                     price={""} 
                     action={""}
                 /> -->
-                <CourseCard 
-                    title={$t('courses.tajwid1.title')} 
-                    desc={$t('courses.tajwid1.desc')} 
-                    src={"/designs/tajwid1.png"} 
-                    level={"1"} 
-                    link={"/courses/tajwid1"} 
-                    price={""} 
-                    action={"now"}
-                />
-                <CourseCard 
-                    title={$t('courses.hadith1.title')} 
-                    desc={$t('courses.hadith1.desc')} 
-                    src={"/designs/hadith1.png"} 
-                    level={"1"} 
-                    link={"/courses/hadith1"} 
-                    price={""} 
-                    action={"later"}
-                />
-        </div>
+      <CourseCard
+        title={$t("courses.tajwid1.title")}
+        desc={$t("courses.tajwid1.desc")}
+        src={"/designs/tajwid1.png"}
+        level={"1"}
+        link={"/courses/tajwid1"}
+        price={""}
+        action={"now"}
+      />
+      <CourseCard
+        title={$t("courses.hadith1.title")}
+        desc={$t("courses.hadith1.desc")}
+        src={"/designs/hadith1.png"}
+        level={"1"}
+        link={"/courses/hadith1"}
+        price={""}
+        action={"later"}
+      />
     </div>
-
+  </div>
 {/if}
 
 <style>
-    div.first-section {
-        display: flex; flex-direction: column; justify-content: center; align-items: center;
-        flex-wrap: wrap;
-    }
-    div.paras-about {
-        min-width: 200px;
-    }
-    img.img-hero-side {
-        max-width: 40vw;
-        min-width: 200px;
-        border-radius: 16px;
-        border: 10px white solid;
-        margin: 16px 56px 56px 72px;
-        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    }
+  div.first-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  div.paras-about {
+    min-width: 200px;
+  }
+  img.img-hero-side {
+    max-width: 40vw;
+    min-width: 200px;
+    border-radius: 16px;
+    border: 10px white solid;
+    margin: 16px 56px 56px 72px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  }
+  div.about {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  @media only screen and (max-width: 1000px) {
     div.about {
-        flex:1; 
-
-display:flex;
-justify-content:center;
-align-items:center;
+      flex-direction: column;
     }
-    @media only screen and (max-width: 1000px) {
-        div.about {
-    flex-direction:column;
   }
-}
-    div.main {
-        min-height: 100vh;
-        min-width: 100vw;
-        max-width: 100vw;
-        /* background-image: ; */
-    }
-    div.gallery {
-  flex:1; 
-
-  display:flex;
-  justify-content:center;
-  align-items:center;
-    }
-    @media only screen and (max-width: 600px) {
+  div.main {
+    min-height: 100vh;
+    min-width: 100vw;
+    max-width: 100vw;
+    /* background-image: ; */
+  }
   div.gallery {
-    flex-direction:column;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 20px;
+    margin-right: 20px;
   }
-}
-
-
-
-    div.paras-about {
-        margin: 32px 86px 56px 56px;
+  @media only screen and (max-width: 600px) {
+    div.gallery {
+      flex-direction: column;
     }
-    p {
-        text-align: "center";
-        font-size: larger;
-    }
-    
+  }
+
+  div.paras-about {
+    margin: 32px 86px 56px 56px;
+  }
+  p {
+    text-align: "center";
+    font-size: larger;
+  }
 </style>
