339dbb0a5ab5146af6f7498c906c5db8c878d779
Naved Islam <navedcoded@gmail.com>
2021-12-12 13:52:57 +0200

    more articles

--- src/elements/articles.svelte
+++ src/elements/articles.svelte
@@ -1,24 +1,40 @@
 <script>
-
 </script>
 
 <container>
-    <h1>Articles</h1>
-<a href="/articles/fard-ayn">The Mandatory Knowledge Every Muslim Must Learn →</a>
+  <h1>Articles</h1>
+  <a target="_blank"  href="/articles/fard-ayn"
+    >The Mandatory Knowledge Every Muslim Must Learn →</a
+  >
+  <a target="_blank"  href="/articles/knowledge-good-intentions"
+    >Good Intentions – Advice for Students of Knowledge 1 →</a
+  >
   <a target="_blank"  href="/articles/curing-laziness" id="about"
+    >Curing Languor and Laziness in Seeking Knowledge →</a
+  >
 </container>
 
-
 <style>
-    container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        background-color: var(--secondary-color);
-        color: var(--text-color);
-        padding: 5vh 0 10vh 0;
-    }
-    a {
-        text-decoration: 2px dashed gold;
-    }
+  container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--secondary-color);
+    color: var(--text-color);
+    padding: 5vh 0 15vh 0;
+    gap: 2vh;
+  }
+  a {
+    border-radius: 8px;
+    border: 2px dashed gold;
+    padding: 10px;
+    background-color: white;
+    box-shadow: rgba(44, 105, 70, 0.2) 0px 2px 8px 0px;
+
+  }
+  a:hover {
+    text-decoration: underline dashed gold 2px;
+    box-shadow: rgba(44, 105, 70, 0.2) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+  }
 </style>
---
+++ src/routes/articles/knowledge-good-intentions.svelte
@@ -0,0 +1,24 @@
+<h1 id="good-intentions">Good Intentions</h1>
+<p><em>Advice for Students of Knowledge 1</em></p>
+<p>The Prophet, peace and blessings be upon him, said, “Actions are only by intention, and for every man is only what he intended.”</p>
+<p>The first obligation upon a student of knowledge is to make a good intention for his knowledge and teaching. A good, sincere intention is the greatest of elixirs — the good and benefit of an action is multiplied in accordance with the plentitude of intentions.</p>
+<p>Some the intentions that a student of knowledge should have are the following:</p>
+<ul>
+<li>Preservation of the Book of Allah and the Sunnah of His Messenger, peace and blessings be upon him</li>
+<li>Obedience to the command of the Beloved, peace and blessings be upon him</li>
+<li>Listening to and transmitting the speech of the Messenger of Allah, peace and blessings be upon him</li>
+<li>Lifting sins from oneself and fellow Muslims by learning and teaching the personal and communal obligations</li>
+<li>Exposure to the winds of Divine Mercy in order to attain His pleasure</li>
+<li>Remembrance and reminder</li>
+<li>Preserving one’s time</li>
+<li>Spreading the sacred teachings of Islam</li>
+<li>I`tikāf (religious seclusion) if the gathering is in the mosque</li>
+<li>Renewal of faith</li>
+<li>Mutual assistance to piety, mindfulness of God, commanding the good, and forbidding the evil</li>
+<li>Increasing the collective body of the people of good</li>
+<li>Seeking the mercy of Allah to descend by remembering the righteous</li>
+<li>Purification of the inward and struggling against the lower self</li>
+<li>Acting according to one’s knowledge</li>
+<li>Oh Allah, make our knowledge, actions, teaching, and the rest of our actions sincere for Your Noble Countenance, by the rank of Your Greatest Beloved, peace and blessings be upon him and his beloveds.</li>
+</ul>
+<p>A servant of this noble knowledge, <br>Abd al-Rahman al-Sha`ār <br>02/21/19</p>
