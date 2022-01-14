ee4ea51bf7eaab8de294f883bddf9ab3a9efa788
Naved Islam <navedcoded@gmail.com>
2022-01-14 13:44:29 +0200

    basic modal

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,18 +1,27 @@
 <script>
+  import { t } from "svelte-intl-precompile";
   import { closeModal } from "svelte-modals";
 
   // provided by Modals
   export let isOpen;
-
-  export let title;
-  export let message;
 </script>
 
 {#if isOpen}
   <div role="dialog" class="modal">
     <div class="contents">
-      <h2>{title}</h2>
-      <p>{message}</p>
+      <h1>{$t("shaykh.long_bio_title")}</h1>
+      <p>{$t("shaykh.long_bio")}</p>
+
+      <h2>{$t("shaykh.long_bio_studies_title")}</h2>
+      <p>{$t("shaykh.long_bio_studies_1")}</p>
+      <p>{$t("shaykh.long_bio_studies_2")}</p>
+      <p>{$t("shaykh.long_bio_studies_3")}</p>
+
+      <h2>{$t("shaykh.long_bio_spirituality_title")}</h2>
+      <p>{$t("shaykh.long_bio_spirituality_1")}</p>
+      <p>{$t("shaykh.long_bio_spirituality_2")}</p>
+      <p>{$t("shaykh.long_bio_spirituality_3")}</p>
+
       <div class="actions">
         <button on:click={closeModal}>OK</button>
       </div>
@@ -21,6 +30,12 @@
 {/if}
 
 <style>
+  h1 {
+    color: black;
+    margin: 0 0 10px 0;
+  }
+  h2 {
+  }
   .modal {
     position: fixed;
     top: 0;
@@ -30,13 +45,15 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 1000;
 
     /* allow click-through to backdrop */
     pointer-events: none;
   }
 
   .contents {
-    min-width: 240px;
+    min-width: 300px;
+    max-width: 800px;
     border-radius: 6px;
     padding: 16px;
     background: white;
@@ -44,11 +61,16 @@
     flex-direction: column;
     justify-content: space-between;
     pointer-events: auto;
+
+    max-height: calc(100vh - 100px);
+    overflow-y: auto;
   }
 
   h2 {
     text-align: center;
     font-size: 24px;
+    margin: 1em 0em 0em 0em;
+    font-weight: bold;
   }
 
   p {
